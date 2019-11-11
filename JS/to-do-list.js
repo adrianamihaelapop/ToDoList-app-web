d0c60c1b27c76d5234754eb375209673e5480680
Pop Adriana <anni.ania19@yahoo.com>
2019-11-11 21:30:04 +0200

    added support for updating and deleting items

--- JS/to-do-list.js
+++ JS/to-do-list.js
@@ -17,10 +17,10 @@ window.ToDoList = {
         let deadlineValue = $("#deadline-field").val();
 
         var requestBody = {
-        description: descriptionValue,
+            description: descriptionValue,
             deadline: deadlineValue
 
-    };
+        };
 
         $.ajax({
             url: ToDoList.API_URL,
@@ -30,9 +30,36 @@ window.ToDoList = {
             data: JSON.stringify(requestBody)
 
 
-        }).done (function () { ToDoList.getItems()
+        }).done(function () {
+            ToDoList.getItems()
 
         })
+    },
+    markItemDone: function (id, done) {
+        let requestBody = {
+            done: done
+        };
+        $.ajax({
+            url: ToDoList.API_URL + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+
+        });
+
+    },
+    deleteItem: function (id,) {
+
+        $.ajax({
+            url: ToDoList.API_URL + "?id=" + id,
+            method: "DELETE",
+        }).done(function () {
+            ToDoList.getItems();
+
+        });
+
     },
 
     displayItems: function (items) {
@@ -61,10 +88,28 @@ window.ToDoList = {
         </tr>`
     },
     bindEvents: function () {
-        $("#create-item-from").submit (function (event) {
+        $("#create-item-from").submit(function (event) {
             event.preventDefault();
             ToDoList.createItem();
-        })
+        });
+        //delegate is necessary because
+        $("#to-do-items").delegate(".delete-item", "click", function (event) {
+            event.preventDefault();
+            let id = $(this).data("id");
+            ToDoList.markItemDone(id, checked);
+
+        });
+
+            ToDoList.deleteItem();
+        });
+        //delegate is necessary because
+        $("#to-do-items").delegate(".mark-done", "change", function (event) {
+            event.preventDefault();
+            let id = $(this).data("id");
+            let checked = $(this).is(":checked");
+            ToDoList.markItemDone(id, checked);
+
+        });
 
     }
 
