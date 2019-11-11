4a2f68e48c336a4cfb88efff1a2f861255895b42
Pop Adriana <anni.ania19@yahoo.com>
2019-11-11 20:18:35 +0200

    displaying item from api in web page

--- JS/to-do-list.js
+++ JS/to-do-list.js
@@ -1,6 +1,6 @@
 window.ToDoList = {
 
-    API_URL: "http://localhost:8081/to-do-item",
+    API_URL: "http://localhost:8081/to-do-items",
 
     getItems: function () {
         $.ajax({
@@ -9,7 +9,34 @@ window.ToDoList = {
         }).done(function (response) {
             console.log("GET done");
             console.log(response);
+            ToDoList.displayItems(JSON.parse(response))
         });
+    },
+
+    displayItems: function (items) {
+        var tableContent = "";
+
+        items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
+
+
+        $("#to-do-items tbody").html(tableContent);
+
+    },
+    getItemTableRow: function (item) {
+        //spread (...)
+        var deadline = new Date(...item.deadline).toLocaleDateString("en");
+
+        //ternary operator
+        var checkedAttribute =item.done ? "checked" : "";
+
+
+        return `<tr>
+            <td>${item.description}</td>
+            <td>${deadline}</td>
+            <td><input type="checkbox" class="mark-done"
+            data-id="${item.id}" ${checkedAttribute} src=""/></td>
+            <td><a href="#" class="delete-item" data-id="${item.id}><i class="fas fa-trash-alt"></i></a></td>
+        </tr>`
     }
 
 };
